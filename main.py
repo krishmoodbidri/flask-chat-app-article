4ddab48dc345d4baeb571a44bc34bd57b964b355
Krish Moodbidri <krish94@uab.edu>
2019-11-05 16:05:17 -0600

    client info - username and msg sent as json object server responds to the request and creates a timestamp.txt .txt file is converted to a .done file with the assumption that .done confirms the account request
    WIP: server sending a message to client to redirect to new page on .done

--- main.py
+++ main.py
@@ -15,30 +15,31 @@ def sessions():
     return render_template('session.html')
 
 
+directory = "/Users/krishmoodbidri/projects/test-flasl/flask-chat-app-article/db"
+if not os.path.exists(directory):
+            os.makedirs(directory)
+
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
+
+@socketio.on('user connect')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
 
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
-
-    directory = "/Users/krishmoodbidri/projects/test-flasl/flask-chat-app-article/db" 
-    if not os.path.exists(directory):
-                os.makedirs(directory)
+    socketio.emit('my response', json, callback=messageReceived)
     time_stamp = time.strftime("%m-%d-%Y_%H:%M:%S")
     complete_file_name = os.path.join(directory, time_stamp + ".txt")
     file = open(complete_file_name, "w")
     file.close()
+    time.sleep( 5 )
+
+
     pre, ext = os.path.splitext(complete_file_name)
     os.rename(complete_file_name, pre + ".done")
-    socketio.emit('my response', json, callback=messageReceived) 
-
-
-
-
-
-    socketio.emit('my response', json, callback=messageReceived)
 
 
 if __name__ == '__main__':
